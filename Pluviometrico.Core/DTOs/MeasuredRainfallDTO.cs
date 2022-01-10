2ba7a662c630f0b2551a7fd43a42e982dcdc589a
rodolfochagas <rodolfochagas@id.uff.br>
2022-01-09 22:07:34 -0300

    Added DTO

---
+++ Pluviometrico.Core/DTOs/MeasuredRainfallDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluviometrico.Core.DTOs
+{
+    public class MeasuredRainfallDTO
+    {
+        public string? Source { get; set; }
+        public string City { get; set; }
+        public string UF { get; set; }
+        public int? Day { get; set; }
+        public int? Month { get; set; }
+        public int? Year { get; set; }
+        public int Hour { get; set; }
+        public string StationCode { get; set; }
+        public string StationName { get; set; }
+        public double? Distance { get; set; }
+        public double RainfallIndex { get; set; }
+    }
+}
