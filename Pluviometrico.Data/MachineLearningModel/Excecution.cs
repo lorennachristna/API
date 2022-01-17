2198ed078074c003fdad6921e0569ac39fcfd5f7
rodolfochagas <rodolfochagas@id.uff.br>
2022-01-16 21:55:36 -0300

    Added Database Table whith Machine Learning results

---
+++ Pluviometrico.Data/MachineLearningModel/Excecution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pluviometrico.Data.MachineLearningModel
+{
+    [Table("execucoes")]
+    public class Excecution
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+
+        [Column("sgbd")]
+        public string DBMS { get; set; }
+
+        [Column("querynumber")]
+        public int QueryNumber { get; set; }
+
+
+        [Column("maquinaaws")]
+        public string AWSMachine { get; set; }
+
+
+        [Column("qtdlinhas")]
+        public int NumberOfLine { get; set; }
+
+
+        [Column("datainicio")]
+        DateTime StartDate { get; set; }
+
+
+        [Column("datafim")]
+        DateTime EndDate { get; set; }
+
+
+        [Column("tempoexecucao")]
+        public int ExecutionTime { get; set; }
+    }
+}
